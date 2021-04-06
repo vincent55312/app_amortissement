a87a416f5e380175da8815ab86c6284d522942df
vvuylsteker <vuylsteker@protonmail.ch>
2021-04-06 13:31:32 +0200

    rounding

--- Amortization.cs
+++ Amortization.cs
@@ -10,11 +10,13 @@ namespace Application_amortissement
         private decimal annuities {get; set;}
         private decimal volume_annuities {get; set;}
         private decimal compatable_value {get; set;}
-        private DateTime start{get; set;}
+        private DateTime start_date{get; set;}
         private int years_duration {get; set;}
         private decimal total_days {get; set;}
         private decimal days_first_year {get; set;}
         private int days_in_year = 365;
+
+
         public DateTime getInputDate(){
             try{
                 Console.Write("Saisir l'année : ");
@@ -29,15 +31,18 @@ namespace Application_amortissement
                 return new DateTime(0,0,0);
             }
         }
+
         public decimal rounding(decimal d){
-            return Math.Round(d,1,MidpointRounding.ToEven);
+            return Math.Round(d,3,MidpointRounding.ToEven);
         }
+
+
         public void Amortisize_linear(){
             Console.Clear();
             Design.printTag();
             try{
                 Design.WriterColor("Saisir date de commencement:", ConsoleColor.White);
-                start = getInputDate();
+                start_date = getInputDate();
                 Design.WriterColor("Saisir la durée d'amortissement (input type int) :", ConsoleColor.Green);
                 years_duration = int.Parse(Console.ReadLine());
                 Design.WriterColor("Saisir la base d'amortissement (input type decimal) :", ConsoleColor.Blue);
@@ -47,15 +52,15 @@ namespace Application_amortissement
                 
                 var table = new ConsoleTable("Années", "Base d’amortissement ", "Annuités", "Cumul des annuités", "Valeur comptable");
 
-                DateTime end_year = new DateTime(start.Year, 12, 31);
-                days_first_year = (end_year - start).Days;
+                DateTime end_year = new DateTime(start_date.Year, 12, 31);
+                days_first_year = (end_year - start_date).Days;
                 total_days = days_first_year + years_duration * days_in_year;
                 tx_linear = 1/(decimal)(total_days/days_in_year);
 
                 int i = 0;
-                Design.WriterColor("Start :" + start.ToString() +" Duration :"+ years_duration+" years  Base amortisize :" + base_amortisize +" Residual value :"+ residual_value +" Tx linear :"+ tx_linear, ConsoleColor.DarkGreen);
+                Design.WriterColor("Start :" + start_date.ToString() +" Duration :"+ years_duration+" years  Base amortisize :" + base_amortisize +" Residual value :"+ residual_value +" Tx linear :"+ tx_linear, ConsoleColor.DarkGreen);
                 while(total_days/days_in_year - i >0){
-                    int year = start.Year + i;
+                    int year = start_date.Year + i;
                     annuities = (base_amortisize-residual_value) * tx_linear;
                     if(i==0) annuities = annuities*(days_first_year/days_in_year);
                     volume_annuities += annuities;
