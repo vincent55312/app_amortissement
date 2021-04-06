efbfe1f3e13ce23c8459fcf211302c419c0ef3aa
vvuylsteker <vuylsteker@protonmail.ch>
2021-04-06 10:43:24 +0200

    Loop menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace Application_amortissement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            sendMenu();
+        }
+
+        private static void sendMenu(){
+            bool showMenu = true;
+            while (showMenu){
+                showMenu = MainMenu();
+            }
+        }
+        public static bool MainMenu()
+        {
+            Console.Clear();
+            Design.printTag();
+            Design.getMenu();
+            switch (Design.getUserKeys().ToUpper())
+            {
+                case "A":
+                    return true;
+                case "B":
+                    return true;
+                case "C":
+                    Design.printTag();
+                    return true;
+                default:
+                    Console.Clear();
+                    Design.printTag();
+                    return true;
+            }
         }
     }
 }
