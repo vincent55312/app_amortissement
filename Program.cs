3258ebf68b586618742c33b39be4d37a1cf868c5
vvuylsteker <vuylsteker@protonmail.ch>
2021-04-06 10:11:43 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application_amortissement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
